517c6e87493a346652cfd95fd2d18088e3a49b7e
elephantfive <dylan.chilcoat@gmail.com>
2025-10-19 08:32:14 -0500

    Player Movement Slows More Quickly
    Player character now decelerates twice as fast as they accelerate for a smoother stop

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -2,12 +2,17 @@ extends CharacterBody2D
 var speed: float = 400.0
 var movement_direction: Vector2
 var acceleration: float = 5.0
+var deceleration: float = 10.0
 
 func _input(event):
 	if event.is_action('movement'):
 		movement_direction.x = Input.get_action_strength('right') - Input.get_action_strength('left')
 		movement_direction.y = Input.get_action_strength('down') - Input.get_action_strength('up')
 
+
 func _physics_process(delta):
-	velocity = velocity.lerp(movement_direction * speed, acceleration * delta)
+	if movement_direction != Vector2(0,0):
+		velocity = velocity.lerp(movement_direction * speed, acceleration * delta)
+	else:
+		velocity = velocity.lerp(movement_direction, deceleration * delta)
 	move_and_slide()
