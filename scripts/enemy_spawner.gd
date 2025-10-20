599e51fffdba32c8a3ccae5a8a4b91e3f9457260
elephantfive <dylan.chilcoat@gmail.com>
2025-10-19 19:23:22 -0500

    Added world boundary collision

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,7 +1,7 @@
 extends Node
 const BASIC_ENEMY = preload("uid://bbxblh6xq6x8j")
 @onready var player = %Player
-
+var iteration: int = 0
 
 func _ready():
 	spawn(BASIC_ENEMY)
@@ -12,3 +12,8 @@ func spawn(enemy):
 	add_child(new_enemy)
 	new_enemy.player = player
 	new_enemy.global_position = Vector2(randf_range(100, get_parent().get_viewport_rect().size.x - 100), randf_range(100, get_parent().get_viewport_rect().size.y - 100))
+
+
+func _on_spawn_timer_timeout():
+	iteration += 1
+	spawn(BASIC_ENEMY)
